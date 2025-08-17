5f12b779fe311a36e74ab40fcdd0cfaf728cd91c
sumitmahankale <sumitmahankale7777@gmail.com>
2025-08-17 13:53:42 +0530

    Added FindMySpot Live Link

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -82,7 +82,7 @@ const ProjectsSection = ({ darkMode = false }) => {
       technologies: ["React.js", "Node.js", "Express.js", "MySQL"],
       icon: <MapPin className="w-5 h-5 sm:w-6 sm:h-6" />,
       githubLink: "https://github.com/sumitmahankale/FindMySpot",
-      liveLink: null,
+      liveLink: "https://find-my-spot-tau.vercel.app/",
       backgroundImage: "findmyspot.png",
       featured: false,
       stats: { stars: 12, forks: 3 }
